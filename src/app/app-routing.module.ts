ada2135b2243d6c854e75756a8dff8b1cb9d717d
Irina <irina.kbts@gmail.com>
2021-07-18 13:13:36 +0300

    Navigation is added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectListComponent } from './project/project-list/project-list.component';
+import { TaskListComponent } from './task/task-list/task-list.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'projects', component: ProjectListComponent},
+  {path: 'users', component: UserListComponent},
+  {path: 'tasks', component: TaskListComponent},
+  { path: '**', redirectTo: 'projects' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app-root/app.component';
+import { ProjectModule } from './project/project.module';
+import { TaskModule } from './task/task.module';
+import { UserModule } from './user/user.module';
+import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProjectModule,
+    UserModule,
+    TaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
